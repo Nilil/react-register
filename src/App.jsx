bfd2a131d5d706d8c1c170f73f6a76c5a581a1e3
Nilil <nilthonec@gmail.com>
2021-09-17 19:35:09 -0500

    Proyect begin

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+import {Badge} from './Badge';
+
+function App(props) {
+  return (
+    <React.Fragment>
+      <Badge/>
+    </React.Fragment>
+  )
+}
+
+export default App;
